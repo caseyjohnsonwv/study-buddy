e7fce7bab95f27d92a8a1a5c90e82674ae2ac061
Casey <caseyjohnsonwv@gmail.com>
2024-01-28 18:26:30 -0500

    add support for pdf and html files

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import gradio as gr
 import env
 from utils.vectordb import VectorDB
 
-COURSE = VectorDB('HMG6596', force_recreate=False)
+COURSE = VectorDB('HMG6228', force_recreate=True)
 CHAT_MODEL = ChatOpenAI(model='gpt-3.5-turbo', temperature=0, api_key=env.OPENAI_API_KEY, organization=env.OPENAI_ORG_ID)
 
 SYSTEM_TEMPLATE = """
     You are a helpful study assistant who examines educational materials to answer questions.
     Given the context below, beneath the triple backticks, answer the human's question.
-    Cite the associated filename and slide number when answering so the human can read further.
+    Cite the associated filename and page or slide number (if applicable) when answering so the human can read further.
     Format outputs into lists and tables as appropriate for readability.
     Always prefer the context below over your pre-existing knowledge.
     ```
@@ -24,6 +24,7 @@ def chat_function(message:str, history:List[Tuple[str, str]]):
     retrieved_docs = COURSE.search(message, k=5)
     if len(retrieved_docs) == 0:
         return 'Sorry, no relevant documents found.'
+    retrieved_docs.sort(key=lambda t:t[1], reverse=True)
     context = '\n'.join([f"Context: {d[0].page_content} | Metadata: {json.dumps(d[0].metadata)}" for d in retrieved_docs])
     # prepare prompt
     messages.append(SystemMessage(content = '\n'.join([SYSTEM_TEMPLATE, context])))
--- utils/vectordb.py
+++ utils/vectordb.py
@@ -4,9 +4,12 @@ import re
 from typing import List, Tuple
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.docstore.document import Document
+from langchain_community.document_loaders.html_bs import BSHTMLLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_openai import OpenAIEmbeddings
 from pptx import Presentation
+import pypdf
 import env
 
 class VectorDB:
@@ -14,6 +17,9 @@ class VectorDB:
     _course_path = None
     _embeddings = None
     _vectorstore = None
+
+    def search(self, phrase:str, k:int=5, metadata_filter:dict={}) -> List[Tuple[Document, float]]:
+        return self._vectorstore.similarity_search_with_score(phrase, k=k, filter=metadata_filter)
 
     def __init__(self, course_name:str, force_recreate:bool=False) -> None:
         self.course_name = course_name
@@ -24,12 +30,23 @@ class VectorDB:
             print(f"Loading existing index from {self._course_path}")
             self._vectorstore = FAISS.load_local(self._course_path, self._embeddings, index_name='index')
         else:
-            print(f"Generating new embeddings from {self._course_path}")
             # otherwise, crawl course materials directory
-            pptx_glob_expr = os.path.normpath(os.path.join(self._course_path, '*', '*.pptx'))
-            pptx_docs = self._load_pptx_docs(pptx_glob_expr)
+            print(f"Generating new embeddings from {self._course_path}")
+            filetype_loader_funcs = {
+                '.pptx': self._load_pptx_docs,
+                '.pdf': self._load_pdf_docs,
+                '.html': self._load_html_docs,
+            }
+            final_docs_list = []
+            for k,v in filetype_loader_funcs.items():
+                if callable(v):
+                    print(f"Using function {v.__name__}() to index filetype {k}")
+                    glob_expr = os.path.normpath(os.path.join(self._course_path, '*', f"*{k}"))
+                    res_docs = v(glob_expr)
+                    print(f"Loaded {len(res_docs)} documents from {k} files")
+                    final_docs_list.extend(res_docs)
             # then create vectorstore from loaded documents
-            self._vectorstore = FAISS.from_documents(pptx_docs, self._embeddings)
+            self._vectorstore = FAISS.from_documents(final_docs_list, self._embeddings)
             self._vectorstore.save_local(self._course_path, index_name='index')
         
     def _load_pptx_docs(self, pptx_glob_expr:str) -> List[Document]:
@@ -51,13 +68,48 @@ class VectorDB:
                 slide_texts.append(slide_notes_cleaned)
                 full_slide_text = ' '.join(slide_texts)
                 # convert slide to document for embedding
-                slide_document = Document(page_content=full_slide_text, metadata={'filename':filename, 'directory':directory, 'slide_number':n+1})
+                metadata = {'filename':filename, 'filetype': '.pptx', 'directory':directory, 'slide_number':n+1}
+                slide_document = Document(page_content=full_slide_text, metadata=metadata)
                 documents.append(slide_document)
+        # split documents with text splitter
+        text_splitter = RecursiveCharacterTextSplitter(separators=['\s+'], is_separator_regex=True, keep_separator=True, chunk_size=300, chunk_overlap=0)
+        docs = text_splitter.split_documents(documents)
+        # return list of documents ready for embedding
+        return docs
+
+    def _load_pdf_docs(self, pdf_glob_expr:str) -> List[Document]:
+        documents = []
+        # load pdf files
+        for f in glob.glob(pdf_glob_expr):
+            filename, directory = os.path.basename(f), os.path.dirname(f)
+            with open(f, 'rb') as pdf:
+                reader = pypdf.PdfReader(pdf)
+                for page in reader.pages:
+                    page_text = page.extract_text()
+                    page_text_cleaned = re.sub('\s+', ' ', page_text)
+                    metadata = {'filename':filename, 'filetype':'.pdf', 'directory':directory, 'page_number':page.page_number}
+                    page_document = Document(page_content=page_text_cleaned, metadata=metadata)
+                    documents.append(page_document)
         # split documents with text splitter
         text_splitter = RecursiveCharacterTextSplitter(separators=['\s+'], is_separator_regex=True, keep_separator=True, chunk_size=500, chunk_overlap=0)
         docs = text_splitter.split_documents(documents)
         # return list of documents ready for embedding
-        return docs    
-
-    def search(self, phrase:str, k:int=5, metadata_filter:dict={}) -> List[Tuple[Document, float]]:
-        return self._vectorstore.similarity_search_with_score(phrase, k=k, filter=metadata_filter)
+        return docs
+    
+    def _load_html_docs(self, html_glob_expr:str) -> List[Document]:
+        documents = []
+        # load html files
+        for f in glob.glob(html_glob_expr):
+            filename, directory = os.path.basename(f), os.path.dirname(f)
+            metadata = {'filename':filename, 'filetype':'.html', 'directory':directory}
+            loader = BSHTMLLoader(f, open_encoding='utf-8', bs_kwargs={'features':'html.parser'})
+            html_docs = loader.load()
+            for d in html_docs:
+                d.metadata = metadata
+            documents.extend(html_docs)
+        # split documents with text splitter
+        text_splitter = RecursiveCharacterTextSplitter(separators=['\s+'], is_separator_regex=True, keep_separator=True, chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(documents)
+        # return list of documents ready for embedding
+        return docs
+    
